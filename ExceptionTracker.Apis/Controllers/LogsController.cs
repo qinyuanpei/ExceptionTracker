790a1bff3ad8762e9ece29fd0ccec65e6853bd86
qinyuanpei <qinyuanpei@163.com>
2018-08-29 14:14:07 +0800

    Let's Work

---
+++ ExceptionTracker.Apis/Controllers/LogsController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace ExceptionTracker.Apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private readonly MongoClient _mongoClient;
+
+        // GET api/logs
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ExceptionTracker.Apis/Models/MongoDBConfig.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ExceptionTracker.Apis.Models
+{
+    public class MongoDBConfig
+    {
+        public string Host { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
