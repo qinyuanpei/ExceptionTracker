d5ca9171190e399118b9484e03f18a1775d3c1a7
payneqin <payneqin@qt-asia.com>
2018-12-14 14:38:18 +0800

    开始定义过滤器

---
+++ ExceptionTracker.Apis/Filters/ApiResultFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using ExceptionTracker.Apis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionTracker.Apis.Filters
+{
+    public class ApiResultFilterAttribute : ActionFilterAttribute
+    {
+        public virtual void OnResultExecuting(ResultExecutingContext context)
+        {
+            
+        }
+    }
+}
---
+++ ExceptionTracker.Apis/Filters/ModelValidationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionTracker.Apis.Filters
+{
+    /// <summary>
+    /// 模型校验过滤器定义
+    /// </summary>
+    public class ModelValidationAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
