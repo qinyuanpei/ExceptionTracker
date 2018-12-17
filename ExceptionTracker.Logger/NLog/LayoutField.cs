470884519094eda3c0162757681beea15fe65fb1
payneqin <payneqin@qt-asia.com>
2018-12-17 16:44:57 +0800

    基于Layout来定义文档模型

---
+++ ExceptionTracker.Logger/NLog/LayoutField.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using NLog.Config;
+
+namespace ExceptionTracker.Logger.NLog
+{
+    [NLogConfigurationItem]
+    public class LayoutField
+    {
+        public LayoutField(string name, string layout, string bsonType = null)
+        {
+            Name = name;
+            Layout = layout;
+            bsonType = bsonType ?? "String";
+        }
+
+        [RequiredParameter]
+        public string Name { get; set; }
+        [RequiredParameter]
+        public string Layout { get; set; }
+        [RequiredParameter]
+        public string BsonType { get; set; }
+    }
+}
---
+++ ExceptionTracker.Logger/NLog/NLogAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using ExceptionTracker.Logger.Models;
+using ExceptionTracker.Logger.NLog;
+using MongoDB.Bson;
+using NLog;
+using NLog.Targets;
+using NLog.Common;
+
+namespace ExceptionTracker.Logger
+{
+    class NLogAdapter
+    {
+
+    }
+
+    [Target("Mongo")]
+    public class MongoTarget : Target
+    {
+
+        /// <summary>
+        /// 终端地址
+        /// </summary>
+        public string EndPoint { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MongoTarget()
+        {
+            
+        }
+
+        public MongoTarget(IEnumerable<LayoutField> fields)
+        {
+
+        }
+
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+
+        }
+
+
+        protected override void Write(IList<AsyncLogEventInfo> logEvents)
+        {
+
+        }
+    }
+}
